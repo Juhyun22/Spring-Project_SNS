82409b13676ccefdd9aabf073da04bced6daf1f9
Juhyun-Bak <sihyun2533@gmail.com>
2022-02-23 14:54:28 +0900

    login&out #7

--- src/main/java/com/sns/user/UserRestController.java
+++ src/main/java/com/sns/user/UserRestController.java
@@ -3,6 +3,9 @@ package com.sns.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sns.common.EncryptUtils;
 import com.sns.user.bo.UserBO;
+import com.sns.user.model.User;
 
 @RequestMapping("/user")
 @RestController
@@ -19,6 +23,11 @@ public class UserRestController {
 	@Autowired
 	private UserBO userBO;
 
+	/**
+	 * 회원 가입 시, 아이디 중복 확인  
+	 * @param loginId
+	 * @return
+	 */
 	@RequestMapping("/is_duplicated_id")
 	public Map<String, Object> isDuplicatedId(
 			@RequestParam("loginId") String loginId) {
@@ -30,6 +39,15 @@ public class UserRestController {
 		return result;
 	}
 	
+	/**
+	 * 회원 가입 기능 - ajax 호출 
+	 * @param loginId
+	 * @param password
+	 * @param name
+	 * @param email
+	 * @return
+	 * @throws Exception
+	 */
 	@PostMapping("/sign_up")
 	public Map<String, Object> signUpForSubmit(
 			@RequestParam("loginId") String loginId,
@@ -53,6 +71,44 @@ public class UserRestController {
 		return result;
 	}
 	
+	/**
+	 * 로그인
+	 * @param loginId
+	 * @param password
+	 * @param request
+	 * @return
+	 * @throws Exception
+	 */
+	@PostMapping("/sign_in")
+	public Map<String, Object> signIn(
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password,
+			HttpServletRequest request) throws Exception {
+		
+		// 비밀번호 암호화
+		String encryptPassword = EncryptUtils.Hashing(password, loginId);
+		
+		// 로그인 id, 암호화 password로 DB에서 select 
+		User user = userBO.getUserByLoginIdPassword(loginId, encryptPassword);
+		
+		// 결과 json return 
+		Map<String, Object> result = new HashMap<>();
+		result.put("result", "success");
+		
+		// 로그인 성공 시 
+		if (user != null) {
+			// 로그인 - 세션에 저장(로그인 상태 유지) 
+			HttpSession session = request.getSession();
+			session.setAttribute("userLoginId", user.getLoginId());
+			session.setAttribute("userId", user.getId());
+			session.setAttribute("userName", user.getName());
+		} else {
+			result.put("result", "error");
+			result.put("errorMessage", "존재하지 않는 사용자입니다. 관리자에게 문의해주세요.");
+		}
+		
+		return result;
+	}
 }
 
 
--- src/main/webapp/WEB-INF/jsp/include/gnb.jsp
+++ src/main/webapp/WEB-INF/jsp/include/gnb.jsp
@@ -1,9 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <div class="d-flex align-items-center h-100 ml-3">
 	<div>
 		<a href="#" id="icon" class="text-monospace text-info">
 			<h2>Gramgram</h2>
 		</a>
+	</div>
+	<div class="d-flex align-items-end">
+		<c:if test="${not empty userName}">
+			<div>${userName}님 안녕하세요.</div>
+			<a href="/user/sign_out">로그아웃</a>
+		</c:if>
 	</div>
 </div>
